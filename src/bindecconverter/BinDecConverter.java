f1668611837cc9c34336d12b9db513573ce98ab7
Befeepilf <befeepilf@protonmail.com>
2016-08-03 21:29:49 +0200

    layout update

--- src/bindecconverter/BinDecConverter.java
+++ src/bindecconverter/BinDecConverter.java
@@ -5,23 +5,35 @@
  */
 package bindecconverter;
 
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 import javafx.application.Application;
+import javafx.beans.InvalidationListener;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
  *
- * @author moritz
+ * @author befeepilf
  */
 public class BinDecConverter extends Application {
+    public static int mode = 0;
     
     @Override
     public void start(Stage primaryStage) {
@@ -30,26 +42,239 @@ public class BinDecConverter extends Application {
         grid.setHgap(10);
         grid.setVgap(10);
         grid.setPadding(new Insets(25, 25, 25, 25));
+        //grid.setGridLinesVisible(true);
         
         Text title = new Text("Binary to Decimal");
         title.setFont(Font.font("Helvetica", 20));
-        grid.add(title, 0, 0);
+        title.setOnMouseReleased(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                ObservableList<Node> nodes = new ObservableList<Node>() {
+                    @Override
+                    public void addListener(ListChangeListener<? super Node> listener) {
+                        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+
+                    @Override
+                    public void removeListener(ListChangeListener<? super Node> listener) {
+                        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+
+                    @Override
+                    public boolean addAll(Node... elements) {
+                        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+
+                    @Override
+                    public boolean setAll(Node... elements) {
+                        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+
+                    @Override
+                    public boolean setAll(Collection<? extends Node> col) {
+                        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+
+                    @Override
+                    public boolean removeAll(Node... elements) {
+                        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+
+                    @Override
+                    public boolean retainAll(Node... elements) {
+                        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+
+                    @Override
+                    public void remove(int from, int to) {
+                        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+
+                    @Override
+                    public int size() {
+                        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+
+                    @Override
+                    public boolean isEmpty() {
+                        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+
+                    @Override
+                    public boolean contains(Object o) {
+                        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+
+                    @Override
+                    public Iterator<Node> iterator() {
+                        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+
+                    @Override
+                    public Object[] toArray() {
+                        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+
+                    @Override
+                    public <T> T[] toArray(T[] a) {
+                        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+
+                    @Override
+                    public boolean add(Node e) {
+                        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+
+                    @Override
+                    public boolean remove(Object o) {
+                        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+
+                    @Override
+                    public boolean containsAll(Collection<?> c) {
+                        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+
+                    @Override
+                    public boolean addAll(Collection<? extends Node> c) {
+                        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+
+                    @Override
+                    public boolean addAll(int index, Collection<? extends Node> c) {
+                        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+
+                    @Override
+                    public boolean removeAll(Collection<?> c) {
+                        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+
+                    @Override
+                    public boolean retainAll(Collection<?> c) {
+                        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+
+                    @Override
+                    public void clear() {
+                        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+
+                    @Override
+                    public Node get(int index) {
+                        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+
+                    @Override
+                    public Node set(int index, Node element) {
+                        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+
+                    @Override
+                    public void add(int index, Node element) {
+                        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+
+                    @Override
+                    public Node remove(int index) {
+                        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+
+                    @Override
+                    public int indexOf(Object o) {
+                        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+
+                    @Override
+                    public int lastIndexOf(Object o) {
+                        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+
+                    @Override
+                    public ListIterator<Node> listIterator() {
+                        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+
+                    @Override
+                    public ListIterator<Node> listIterator(int index) {
+                        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+
+                    @Override
+                    public List<Node> subList(int fromIndex, int toIndex) {
+                        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+
+                    @Override
+                    public void addListener(InvalidationListener listener) {
+                        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+
+                    @Override
+                    public void removeListener(InvalidationListener listener) {
+                        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+                };
+                nodes = grid.getChildren();
+                switch (mode) {
+                    case 0:
+                        title.setText("Decimal to Binary");
+                        removeInputs(nodes);
+                        addBinary(grid, 2);
+                        addDecimal(grid, 1);
+                        mode = 1;
+                        break;
+                    case 1:
+                        title.setText("Binary to Decimal");
+                        removeInputs(nodes);
+                        addBinary(grid, 1);
+                        addDecimal(grid, 2);
+                        mode = 0;
+                        break;
+                    default:
+                        System.err.println("unknown mode: " + mode);
+                        System.exit(0);
+                }
+            }
+            
+        });
+        grid.add(title, 0, 0, 2, 1);
         
-        TextField binaryTF = new TextField();
-        grid.add(binaryTF, 1, 1);
+        addBinary(grid, 1);
         
-        Label binaryLBL = new Label("binary");
-        grid.add(binaryLBL, 0, 1);
+        addDecimal(grid, 2);
         
-        Button button = new Button();
-        button.setText("Convert");
-        grid.add(button, 0, 2);
+        Button button = new Button("Convert");
+        
+        HBox hbBtn = new HBox();
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(button);
+        grid.add(hbBtn, 1, 3);
         
         Scene scene = new Scene(grid, 250, 250);
         
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("Binary & Decimal Converter");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
     
+    public void addBinary(GridPane grid, int row) {
+        Label binaryLBL = new Label("binary");
+        grid.add(binaryLBL, 0, row);
+        
+        TextField binaryTF = new TextField();
+        grid.add(binaryTF, 1, row);
+    }
+    
+    public void addDecimal(GridPane grid, int row) {
+        Label decimalLBL = new Label("decimal");
+        grid.add(decimalLBL, 0, row);
+        
+        TextField decimalTF = new TextField();
+        grid.add(decimalTF, 1, row);
+    }
+    
+    public void removeInputs(ObservableList<Node> nodes) {
+        nodes.remove(1, 5);
+    }
 }
